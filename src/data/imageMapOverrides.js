b11d111cea10db4050eab9b4eb0abdaf19323c05
VictorChaoss <iaksaray@outlook.com>
2026-08-17 16:52:50 +0100

    Add CJ (Carl Johnson) coin and icon

--- src/data/imageMapOverrides.js
+++ src/data/imageMapOverrides.js
@@ -5,5 +5,6 @@ stockImageMap["BOM"] = "/explore_photos/Bmlogo-GTAV.webp";
 stockImageMap["LSC"] = "/explore_photos/GTAV-LSCustoms-Logo.webp";
 stockImageMap["UMA"] = "/explore_photos/Revolter-GTAV-detail.webp";
 stockImageMap["SUB"] = "/explore_photos/Stock-SUB.webp";
+stockImageMap["CJ"] = "/explore_photos/CJ.jpg";
 
 export { stockImageMap };
