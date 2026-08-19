b1011e19d2f3976702dd5e65706b6fd5a2d74bdf
VictorChaoss <iaksaray@outlook.com>
2026-08-19 13:11:16 +0100

    Restore GTA token to unregulatedStocks

--- src/data/stocks.js
+++ src/data/stocks.js
@@ -158,12 +158,28 @@ export const unregulatedStocks = [
     desc: 'Macca The Gator is a community-driven meme coin based on the popular character from the Grand Theft Auto universe.',
   }),
   makeStock({
+    ticker: 'GTA',
+    name: 'GTA',
+    exchange: 'PUMP',
+    sector: 'Unregulated',
+    contract: 'BVEaDToN2Ufn8m1Jnqy8rtwwCiiMC7JcFtvmKPxTpump',
+    desc: 'A community-driven meme token named after the iconic Grand Theft Auto franchise, launched on Pump.fun.',
+  }),
+  makeStock({
     ticker: 'LEONIDA',
     name: 'Leonida - GTA 6 Map',
     exchange: 'PUMP',
     sector: 'Unregulated',
     contract: 'FHhPcCkRZRbyaKQn6uN1Uf5jYadvnFzohGxjWmSHpump',
     desc: 'Leonida is a community-driven GTA 6 Map token launched on Pump.fun.',
+  }),
+  makeStock({
+    ticker: 'GTA',
+    name: 'GTA - Grand Theft Auto',
+    exchange: 'PUMP',
+    sector: 'Unregulated',
+    contract: '2wMTr4ZttVr5tmS4wLStee59wi8f9QQfMtT2Hoispump',
+    desc: 'A community-driven coin where the rewards go to the creator Mike Dailly, the legendary game programmer and creator of Lemmings and Grand Theft Auto.',
   })
 ].map(s => ({ ...s, unregulated: true }));
 export const mockStocks = [
